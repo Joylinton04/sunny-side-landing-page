52777867e7e9ef73f74994c5d3ba2e2e15817188
JoyLinton04 <adujoy05@gmail.com>
2023-10-23 11:45:27 +0000

    Added a mobile menu

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -2,9 +2,23 @@ import imageHeader from '../assets/desktop/image-header.jpg'
 import iconArrow from '../assets/icon-arrow-down.svg'
 import useWindowSize from '../hooks/useWindowSize'
 import hamburgerIcon from '../assets/icon-hamburger.svg'
+import { useEffect, useRef } from 'react'
 
 const Nav = () => {
     const {width} = useWindowSize()
+    const ref = useRef()
+
+    function handleMobileMenu () {
+        if (ref.current.classList.contains('mb-screen')) {
+            ref.current.classList.remove('mb-screen');
+            ref.current.style.display = "none"
+          } else {
+            ref.current.classList.add('mb-screen');
+            ref.current.style.display = "block"
+          }
+          
+    }
+
   return (
     <nav>        
         <div className='header-img'>
@@ -13,15 +27,25 @@ const Nav = () => {
         <div className='nav-info'>
             <h1>sunnyside</h1>
             {width > 715 ?
-                <ul>
+                <ul className='lr-screen'>
                     <li>about</li>
                     <li>services</li>
                     <li>projects</li>
                     <li>contact</li>
                 </ul>
                : <div>
-                <button className='humburger-icon'><img src={hamburgerIcon} alt=""/></button>
-               </div> 
+                    <button className='humburger-icon' onClick={handleMobileMenu}>
+                        <img src={hamburgerIcon} alt=""/>
+                    </button>
+                    <div className='mb-screen' ref={ref}>
+                        <ul className='mb-screen-menu'>
+                            <li>about</li>
+                            <li>services</li>
+                            <li>projects</li>
+                            <li>contact</li>
+                        </ul>
+                    </div>
+               </div>
             }
         </div>
         <div className='header-mess'>
